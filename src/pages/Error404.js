d4765d5fee20cb140f51ae89fa4bb32bc4739700
Omar YAYA <47383263+omaryaya@users.noreply.github.com>
2020-09-14 22:37:55 +0200

    Initial Commit.

---
+++ src/pages/Error404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from 'typography';
+
+function Error404(props) {
+    return (
+        <div>
+            <h1>404 NOT FOUND.</h1>
+        </div>
+    );
+}
+
+export default Error404;
